e7c38683bee61fc7665b86e39028fbcae3076bba
Penina Wanjiru <penina.wanjiru@andela.com>
2016-08-05 11:43:28 +0300

    [Feature] create main app models

--- main/models.py
+++ main/models.py
@@ -3,3 +3,20 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class ShoppingList(models.Model):
+
+    title = models.CharField(max_length=255)
+    user = models.ForeignKey(User, related_name="user")
+
+    def __unicode__(self):
+        return u'%s' % self.title
+
+class ShoppingListItem(models.Model):
+
+    listitem = models.CharField(max_length=255)
+    bought = models.BooleanField()
+    shoppinglist = models.ForeignKey(ShoppingList, related_name='items')
+
+    def __unicode__(self):
+        return u'%s' % self.listitem
